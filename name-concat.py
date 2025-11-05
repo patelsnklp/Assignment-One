d85eca01c6025ad295e70fd5d8274430ee0c8c19
patelsnklp <patelsnklp@gmail.com>
2025-11-05 16:37:17 +0530

    Upload file

---
+++ name-concat.py
@@ -0,0 +1,10 @@
+#Ask user for name and add a greeting text
+
+
+a = str(input("Enter first name: "))
+b = str(input("Enter last name: "))
+
+name = (a + " " + b)
+
+print("Hello,", name , "!", "Welcome to the Python program.")
+
